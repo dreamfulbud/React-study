75176e46bcf47f9e2a47d9523295fa721131a15c
강혜진 <dreamfulbud@naver.com>
2022-02-21 23:31:11 +0900

    ✨할 일 목록: Map 메소드를 사용한 할 일 목록 나열

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+//클래스형
+export default class App extends Component {
+	btnStyle = {
+		color: "#fff",
+		border: "none",
+		padding: "5px 9px",
+		borderRadius: "50%",
+		cursor: "pointer",
+		float: "right",
+	};
+	getStyle = () => {
+		return {
+			padding: "10px",
+			borderBottom: "1px dotted #ddd",
+		};
+	};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	todoData = [
+		{
+			id: "1",
+			title: "공부하기",
+			completed: true,
+		},
+		{
+			id: "2",
+			title: "청소하기",
+			completed: false,
+		},
+		{
+			id: "3",
+			title: "운동하기",
+			completed: false,
+		},
+	];
+	render() {
+		return (
+			<div className="container">
+				<div className="todoBlock">
+					<h1>할 일 목록</h1>
+
+					<ul>
+						{this.todoData.map((data) => (
+							<>
+								<li style={this.getStyle()} key={data.id}>
+									<input type="checkbox" defaultChecked={data.completed} />
+									{data.title}
+									<button style={this.btnStyle}>x</button>
+								</li>
+							</>
+						))}
+					</ul>
+				</div>
+			</div>
+		);
+	}
 }
-
-export default App;
