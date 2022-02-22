2a6c89dc76ea145bdbed1d008067e178987cf2d3
강혜진 <dreamfulbud@naver.com>
2022-02-22 16:59:18 +0900

    ✨할 일 목록 입력 및 완료 표시 기능 추가

--- src/App.js
+++ src/App.js
@@ -10,30 +10,68 @@ export default class App extends Component {
 		cursor: "pointer",
 		float: "right",
 	};
-	getStyle = () => {
+
+	listStyle = (completed) => {
 		return {
 			padding: "10px",
 			borderBottom: "1px dotted #ddd",
+			textDecoration: completed ? "line-through" : "none",
 		};
 	};
 
-	todoData = [
-		{
-			id: "1",
-			title: "공부하기",
-			completed: true,
-		},
-		{
-			id: "2",
-			title: "청소하기",
+	state = {
+		todoData: [
+			{
+				id: "1",
+				title: "공부하기",
+				completed: true,
+			},
+			{
+				id: "2",
+				title: "청소하기",
+				completed: false,
+			},
+			{
+				id: "3",
+				title: "운동하기",
+				completed: false,
+			},
+		],
+		value: "",
+	};
+
+	handleClick = (id) => {
+		let newTodoData = this.state.todoData.filter((data) => data.id !== id);
+		this.setState({ todoData: newTodoData });
+	};
+
+	handleChange = (e) => {
+		this.setState({ value: e.target.value });
+	};
+
+	// 할 일 입력
+	handleSubmit = (e) => {
+		e.preventDefault();
+
+		let newTodo = {
+			id: Date.now(),
+			title: this.state.value,
 			completed: false,
-		},
-		{
-			id: "3",
-			title: "운동하기",
-			completed: false,
-		},
-	];
+		};
+		this.setState({ todoData: [...this.state.todoData, newTodo], value: "" });
+	};
+
+	// 완료여부 false/true
+	handleCompleChange = (id) => {
+		let newTodoData = this.state.todoData.map((data) => {
+			if (data.id === id) {
+				data.completed = !data.completed;
+			}
+			return data;
+		});
+		this.setState({ todoData: newTodoData });
+	};
+
 	render() {
 		return (
 			<div className="container">
@@ -41,16 +79,23 @@ export default class App extends Component {
 					<h1>할 일 목록</h1>
 
 					<ul>
-						{this.todoData.map((data) => (
+						{this.state.todoData.map((data) => (
 							<>
-								<li style={this.getStyle()} key={data.id}>
-									<input type="checkbox" defaultChecked={data.completed} />
-									{data.title}
-									<button style={this.btnStyle}>x</button>
+								<li style={this.listStyle(data.completed)} key={data.id}>
+									<input type="checkbox" onChange={() => this.handleCompleChange(data.id)} defaultChecked={data.completed} id={"input" + data.id} />
+									<label for={"input" + data.id}>{data.title}</label>
+									<button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>
+										x
+									</button>
 								</li>
 							</>
 						))}
 					</ul>
+
+					<form style={{ display: "flex" }} onSubmit={this.handleSubmit}>
+						<input type="text" name="value" placeholder="해야할 일을 입력하세요." value={this.state.value} onChange={this.handleChange} style={{ flex: "10", padding: "5px" }} />
+						<input type="submit" value="입력" className="btn" style={{ flex: "1" }} />
+					</form>
 				</div>
 			</div>
 		);
