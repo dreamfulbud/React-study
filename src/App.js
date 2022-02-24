9b2ef9408c386eac6151ec299a076e40f0885b81
강혜진 <dreamfulbud@naver.com>
2022-02-24 16:10:34 +0900

    ✅타이머,단위변환,글자수 계산 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import "./App.css";
 import List from "./components/List";
+import TextCounter from "./components/TextCounter";
+import Timer from "./components/Timer";
+import Unit from "./components/Unit";
 //클래스형
 export default function App() {
 	const [todoData, setTodoData] = useState([
@@ -40,19 +43,24 @@ export default function App() {
 	};
 
 	return (
-		<div className="container">
-			<div className="todoBlock">
-				<h1>할 일 목록</h1>
+		<>
+			<Timer />
+			<Unit />
+			<TextCounter />
+			<div className="container">
+				<div className="todoBlock">
+					<h1>할 일 목록</h1>
 
-				<ul>
-					<List todoData={todoData} setTodoData={setTodoData} />
-				</ul>
+					<ul>
+						<List todoData={todoData} setTodoData={setTodoData} />
+					</ul>
 
-				<form style={{ display: "flex" }} onSubmit={handleSubmit}>
-					<input type="text" name="value" placeholder="해야할 일을 입력하세요." value={value} onChange={handleChange} style={{ flex: "10", padding: "5px" }} />
-					<input type="submit" value="입력" className="btn" style={{ flex: "1" }} />
-				</form>
+					<form style={{ display: "flex" }} onSubmit={handleSubmit}>
+						<input type="text" name="value" placeholder="해야할 일을 입력하세요." value={value} onChange={handleChange} style={{ flex: "10", padding: "5px" }} />
+						<input type="submit" value="입력" className="btn" style={{ flex: "1" }} />
+					</form>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
