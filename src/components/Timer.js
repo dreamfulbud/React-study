f9bfce919972457dee5dc8188288a585412793c9
강혜진 <dreamfulbud@naver.com>
2022-02-26 23:36:38 +0900

    ✏️불필요한 텍스트 삭제

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 
 export default function Timer() {
 	const [timer, setTimer] = useState(0);
