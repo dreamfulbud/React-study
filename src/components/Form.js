74c1443c095a022da592754f77b1e04dba6005b1
강혜진 <dreamfulbud@naver.com>
2022-03-05 00:51:24 +0900

    ✅ input 텍스트 길이 제한

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,7 +8,7 @@ export default function Form({ handleSubmit, value, setValue }) {
 
 	return (
 		<form onSubmit={handleSubmit}>
-			<input type="text" name="value" placeholder="해야할 일을 입력하세요." value={value} onChange={handleChange} />
+			<input type="text" name="value" placeholder="해야할 일을 입력하세요." value={value} onChange={handleChange} maxLength={100} />
 			<input type="submit" value="입력" />
 		</form>
 	);
